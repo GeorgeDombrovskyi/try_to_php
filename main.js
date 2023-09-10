ea8eb345cb5a5a7d51a2e5ad9f3c9b468ea3d87e
GeorgeDombrovskyi <georgeDombrovskyi@gmail.com>
2023-09-10 17:55:57 +0300

    first load

---
+++ main.js
@@ -0,0 +1,19 @@
+let ourButton =document.getElementById('ourButton')
+
+ourButton.addEventListener('click', buttonFunction)
+
+function buttonFunction() {
+    alert('yeap! It\'s worked!')
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "your_php_script.php", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Handle the response from the PHP script
+        var response = xhr.responseText;
+        console.log("PHP response: " + response);
+      }
+    };
+    xhr.send();
+  };
+
---
+++ your_php_script.php
@@ -0,0 +1,8 @@
+<?php
+// Your PHP code here
+// This code will execute when the AJAX request is made
+
+// For example, you can perform database operations, file handling, or any other server-side tasks
+// Return a response to the JavaScript code if needed
+echo "PHP code executed!";
+?>
